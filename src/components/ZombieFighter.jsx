c67930a921e4bb8ba3a11523b790b0fe6c2ecc90
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2024-06-23 12:59:23 -0400

    add handleAddFighter function

--- src/components/ZombieFighter.jsx
+++ src/components/ZombieFighter.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
 function ZombieFighter(props) {
-	return (
+  function handleAddFighter() {
+		if (props.money - props.price >= 0) {
+			props.setMoney(props.money - props.price);
+			const newTeam = [...props.team, props];
+			props.setTeam(newTeam);
+		} else {
+			console.log('Not enought money');
+		}
+  }
+
+  return (
 		<li>
 			<img src={props.img} />
 			<p>{props.name}</p>
 			<p>Price: {props.price}</p>
 			<p>Strength: {props.strength}</p>
 			<p>Agility: {props.agility}</p>
-			<button>Add</button>
+			<button onClick={handleAddFighter}>Add</button>
 		</li>
-	);
+  );
 }
 
 export default ZombieFighter;
